cfd79f6db8a0338913ea4a2f0499ad1251ffa20a
TonyDon <hongdeviltxd@163.com>
2014-10-31 12:07:43 +0800

    update test

--- src/test/java/com/uuola/txcms/util/test/SeqTest.java
+++ src/test/java/com/uuola/txcms/util/test/SeqTest.java
@@ -6,7 +6,9 @@
 
 package com.uuola.txcms.util.test;
 
+import com.uuola.commons.DateUtil;
 import com.uuola.commons.coder.SequenceBuilder;
+import com.uuola.commons.constant.CST_DATE_FORMAT;
 
 
 /**
@@ -20,6 +22,7 @@ public class SeqTest {
 
     public static void main(String... args){
         System.out.println(System.currentTimeMillis());
+        System.out.println(DateUtil.parseTimeMillis("2014-11-01 12:12:12", CST_DATE_FORMAT.YYYY_MM_DD_HH_MM_SS));
         long fixedValue = 1414424012812L;
         SequenceBuilder sb = new SequenceBuilder(fixedValue, 9);
         for(int k=0; k<20; k++){
