41c39180dfd0e44ef8217684b8d44025907285e5
TonyDon <hongdeviltxd@163.com>
2014-11-21 23:14:15 +0800

    codec test

--- src/test/java/com/uuola/txcms/util/test/CodecTest.java
+++ src/test/java/com/uuola/txcms/util/test/CodecTest.java
@@ -6,12 +6,17 @@
 
 package com.uuola.txcms.util.test;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.Crypt;
 import org.apache.commons.codec.digest.DigestUtils;
 
-import com.uuola.commons.coder.ByteHexUtil;
 import com.uuola.commons.coder.MyBase64;
 import com.uuola.commons.coder.SHA;
 
@@ -39,6 +44,27 @@ public class CodecTest {
         System.out.println(MyBase64.decode("DGVZDO1tiO1tiB=="));
         
         System.out.println(Crypt.crypt("abc"));
+        
+        try {
+            fileSHA1();
+            fileMD5();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    
+    private static void fileSHA1() throws FileNotFoundException, IOException{
+        File inputFile = new File("E:/Files/go1.3.3.windows-386.zip");
+        String sha1Hex = DigestUtils.sha1Hex(new BufferedInputStream(new FileInputStream(inputFile)));
+        System.out.println(sha1Hex);
+    }
+    
+    private static void fileMD5() throws FileNotFoundException, IOException{
+        File inputFile = new File("E:/Files/go1.3.3.windows-386.zip");
+        String hex = DigestUtils.md5Hex(new BufferedInputStream(new FileInputStream(inputFile)));
+        System.out.println(hex);
     }
 
 }
