010f1c5a0761f4e35deaae4578076088f644e849
TonyDon <hongdeviltxd@163.com>
2015-01-18 15:40:48 +0800

    update

---
+++ src/main/java/com/uuola/commons/http/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * @(#)package-info.java 2015年1月18日
+ * 
+ * Copy Right@ uuola
+ */
+
+/**
+ * <pre>
+ *
+ * @author tangxiaodong
+ * 创建日期: 2015年1月18日
+ * </pre>
+ */
+package com.uuola.commons.http;
